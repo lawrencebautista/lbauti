f7a670ee17ddd5b0057a3831d51c9907d53c2dab
Lawrence Bautista <lawrence1315@gmail.com>
2013-06-12 11:20:04 -0400

    Initial version of the site

---
+++ edm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gak 2: EDM Producer</title>
+</head>
+
+<body>
+<p>Welcome to my EDM page</p>
+<audio controls>
+	<source src="music/oscillot.mp3" type="audio/mpeg">
+	Get a better browser.
+</audio>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="template.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+	<title>Lawrence Bautista</title>
+</head>
+
+<body>
+<header>
+	<a href="index.php" id="homelink"><h1>Lawrence Bautista</h1></a>
+</header>
+
+<nav>	
+	<ul>
+		<li><a class="menu" href="index.php">Home</a></li>
+		<li><a class="menu" href="edm.php">EDM</a></li>
+		<li><a class="menu" href="classical.php">Classical</a></li>
+		<li><a class="menu" href="computers.php">Computers</a></li>
+		<li><a class="menu" href="about.php">About</a></li>
+		<li><a class="menu" href="contact.php">Contact</a></li>
+		<li><a class="menu" href="info.php">Site Info</a></li>
+	</ul>
+</nav>
+
+<aside>
+	<p>Obligatory Social Media Links</p>
+	<ul>
+		<li>Facebook</li>
+		<li>Twitter</li>
+		<li>Google+</i>
+		<li>Reddit</li>
+		<li>Soundcloud</li>
+		<li>Myspace</li>
+	</ul>
+</aside>
+
+<section>	
+	<h3>Finding the path ahead...</h3>
+	<p>It's a little late, but I think I'm ready to venture into the outside world. I hope that this is just an echo of what I'm going to accomplish in the future.</p>
+	<h3>Bio</h3>
+	<p>I am a CS/Bio graduate of McGill University. I've taken a lot of courses such as Artificial Intelligence, Operating Systems, Algorithms and Data Structures, and Computer Networks. Having lived in Montreal and Mississauga, I want to someday live somewhere else in the world</p> 
+	<p>Playing piano is one of my favourite hobbies. I'm currently scheduled to do my ARCT in Piano Performance this coming August.</p>
+	<p>I also play guitar and produce my own EDM music. I will show my projects in the future</p>
+	<p>Enjoy this site, and <a href="mailto:lawrence1315@gmail.com?Subject=Update%20Your%20Site!">remind me</a> to update it!</p>
+</section>
+
+<footer>
+	<p>Website programmed by Lawrence Bautista. &copy;2013. All rights reserved.</p>
+	<p>This is coded in HTML5 and CSS3. Please upgrade to the latest version of your browser if you haven't already</p>
+</footer>
+
+</body>
+</html>
